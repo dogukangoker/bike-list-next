734b6cb78ae63948e9f0723b315755883c1f4981
Doğukan <dogukangkr1234@gmail.com>
2024-05-19 14:30:28 +0300

    fix empty query data

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -39,11 +39,11 @@ export const TableBody = <T extends unknown>({
   const rows = data?.map((item, index) => renderRow(item, index));
 
   const renderBody = () => {
-    if (error)
+    if (error || !rows)
       return (
         <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
           <td className="px-6 py-3 text-center text-3xl" colSpan={6}>
-            No data found. We will try to receive data.
+            No data found.
           </td>
         </tr>
       );
